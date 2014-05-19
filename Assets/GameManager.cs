a49b8e98da22bdc694003a91e39c57d66792d806
leo mulligan <ljmulligan@eircom.net>
2014-05-19 21:27:23 +0100

    class test
    unity class test

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		//GameObject new Bot = Instantiate(bot,transform.position,bot.transform.rotation) as GameObject;
+        GameObject bot = GameObject.FindGameObjectWithTag("bot");
+		GameObject bot7 = GameObject.FindGameObjectWithTag("bot");
+		GameObject bot3 = GameObject.FindGameObjectWithTag("bot");
+		bot.renderer.material.color = Color.blue;
+		bot7.renderer.material.color = Color.blue;
+
+		GameObject ammo = GameObject.FindGameObjectWithTag("ammo");
+		ammo.renderer.material.color = Color.red;
+
+
+		//GameObject newAmmo = Instantiate(ammo,transform.position,ammo.transform.rotation) as GameObject;
+
+		bot.GetComponent<StateMachine>().SwitchState(new IdleState(bot, bot7));
+
+		ammo.renderer.material.color = Color.red;
+		bot3.renderer.material.color = Color.blue;
+
+        
+	}	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Assets/addAmmo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addAmmo : MonoBehaviour 
+{
+	public static int ammo = 0;
+
+
+	void Start ()
+	{
+		collider.isTrigger = true;
+	}
+
+	void OnTriggerEnter( Collider WhatEnteredTheTrigger )
+	{
+		if( WhatEnteredTheTrigger.tag == "bot" )
+		{
+			int ammo = 10;
+		}
+	}
+
+
+		void OnGUI()
+		{
+			string ammoText = "Ammo: " + ammo;
+			GUI.Box (new Rect(Screen.width - 150,40,120,40),ammoText);
+
+		}
+
+}
